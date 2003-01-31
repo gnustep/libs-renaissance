ecfa9d4c2ae58083cee7aca86d0939a78193c510
Nicola Pero <n.pero@mi.flashnet.it>
2003-01-31 03:28:30 +0000

    Regenerated
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/renaissance/trunk@15800 72102866-910b-0410-8b05-ffd578937521

--- Documentation/Pregenerated/labels.pl
+++ Documentation/Pregenerated/labels.pl
@@ -2,12 +2,28 @@
 # Associate labels original text with physical files.
 
 
+$key = q/integrating-renaissance-menu/;
+$external_labels{$key} = "$URL/" . q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/top-level-objects/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
 1;
 
 
 # LaTeX2HTML 2K.1beta (1.48)
 # labels from external_latex_labels array.
 
+
+$key = q/integrating-renaissance-menu/;
+$external_latex_labels{$key} = q|2.6.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/top-level-objects/;
+$external_latex_labels{$key} = q|2.5.3|; 
+$noresave{$key} = "$nosave";
 
 $key = q/XML-entities/;
 $external_latex_labels{$key} = q|2.1|; 
