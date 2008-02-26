50ea24d12ce41b70dfb9dd213ac18ef8d274973f
Nicola Pero <nicola.pero@meta-innovation.com>
2008-02-26 03:37:59 +0000

    Documented the <view> tag, and some documentation tidyups
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/renaissance/trunk@26142 72102866-910b-0410-8b05-ffd578937521

--- Documentation/Pregenerated/labels.pl
+++ Documentation/Pregenerated/labels.pl
@@ -1,4 +1,4 @@
-# LaTeX2HTML 2K.1beta (1.48)
+# LaTeX2HTML 2002-2-1 (1.71)
 # Associate labels original text with physical files.
 
 
@@ -6,18 +6,18 @@ $key = q/autolayout-flags/;
 $external_labels{$key} = "$URL/" . q|node11.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/integrating-renaissance-menu/;
-$external_labels{$key} = "$URL/" . q|node20.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/top-level-objects/;
 $external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/integrating-renaissance-menu/;
+$external_labels{$key} = "$URL/" . q|node20.html|; 
 $noresave{$key} = "$nosave";
 
 1;
 
 
-# LaTeX2HTML 2K.1beta (1.48)
+# LaTeX2HTML 2002-2-1 (1.71)
 # labels from external_latex_labels array.
 
 
@@ -25,16 +25,16 @@ $key = q/autolayout-flags/;
 $external_latex_labels{$key} = q|1.9|; 
 $noresave{$key} = "$nosave";
 
-$key = q/integrating-renaissance-menu/;
-$external_latex_labels{$key} = q|2.6.2|; 
-$noresave{$key} = "$nosave";
-
 $key = q/top-level-objects/;
-$external_latex_labels{$key} = q|2.5.3|; 
+$external_latex_labels{$key} = q|2.5.4|; 
 $noresave{$key} = "$nosave";
 
 $key = q/XML-entities/;
 $external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/integrating-renaissance-menu/;
+$external_latex_labels{$key} = q|2.6.2|; 
 $noresave{$key} = "$nosave";
 
 1;
